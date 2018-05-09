191b2f24899b81ed4f923d279197e8e4bbc7be4b
samskarvan <samskarvan@gmail.com>
2018-05-09 12:27:42 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    <title>PHP usesr Auth Demo</title>
+</head>
+<body>
+    <header class="container">
+    <h1 class="center">This is an H1 taaaaaagg</h1>
+    </header>
+</body>
+</html>
